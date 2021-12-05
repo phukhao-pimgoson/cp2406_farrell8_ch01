78f36a47406a2b996cfd4691bfaa6069a38e1cd7
Phukhao Pimgoson <62372054+phukhao-pimgoson@users.noreply.github.com>
2021-12-05 21:58:55 +0800

    tasked

---
+++ CodingExercises/FavouriteSong.java
@@ -0,0 +1,10 @@
+class FavoriteSong
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Oyasumi, oyasumi");
+        System.out.println("Close your eyes and you'll leave this dream");
+        System.out.println("Oyasumi, oyasumi");
+        System.out.println("I know that its hard to do");
+    }
+}
